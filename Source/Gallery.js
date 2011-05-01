96daeb8f7f3af24c9b97018e96fa6120c8c4cb2a
Ger Hobbelt <ger@hobbelt.com>
2011-05-01 13:26:38 +0200

    correct CSS+JS to accommodate large-ish gallery collections in the Gallery.js (a.k.a. gallery mode): account for the Y scrollbar when galleries take more than 2 rows to display. As we don't resize every time, we account for the scrollbar from the start.

--- Source/Gallery.js
+++ Source/Gallery.js
@@ -257,8 +257,10 @@ FileManager.Gallery = new Class({
 		// determine how many images we can show next to one another:
 		var galWidthComp = 2;    // 2px not accounted for in the Width calculation; without these the width slowly grows instead of 'jump' to fit one more/less image horizontally
 		var imgMarginWidth = 4;  // 4px unaccounted for in the element width
-		var radiiCorr = 20;      // correction for the radii in the FileManager container: looks better that way
+		var radiiCorr = 40;      // correction for the radii in the FileManager container: looks better that way; must at least be 20
 		var imgWidth = 84;       // derived from current CSS; bad form to do it this way, but this is a good guess when there's no image in there yet.
+		var scrollbarCorr = 20;  // correction for optional Y scrollbar when the number of images doesn't fit in two rows anymore.
+
 		var imgs = this.galleryContainer.getElements('li');
 		if (imgs && imgs[0])
 		{
@@ -266,10 +268,10 @@ FileManager.Gallery = new Class({
 			imgWidth = imgs[0].getWidth() + imgMarginWidth;
 		}
 		var galWidth = this.galleryContainer.getWidth();
-		var g_rem = galWidth % imgWidth;
-		var img_cnt = Math.floor((size.x - g_rem - radiiCorr) / imgWidth);
+		var g_rem = (galWidth - scrollbarCorr) % imgWidth;
+		var img_cnt = Math.floor((size.x - g_rem - scrollbarCorr - radiiCorr) / imgWidth);
 		if (img_cnt < 3) img_cnt = 3;
-		var gw = img_cnt * imgWidth + g_rem;
+		var gw = img_cnt * imgWidth + g_rem + scrollbarCorr;
 
 		this.galleryContainer.setStyles({
 			top: pos.y + size.y - 1,
